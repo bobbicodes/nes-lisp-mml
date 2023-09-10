a862abf021ff85e0c84bfe6c3ac25431621f713a
Bobbi Towers <btowers793@gmail.com>
2023-09-10 05:06:49 -0700

    button to start audio context

--- game.clj
+++ game.clj
@@ -1,1 +1,1 @@
-(map str [1 2] [3 4])
+(str "hello " "kitty")
--- main.js
+++ main.js
@@ -3,6 +3,14 @@ import { evalString } from 'bobbi-lisp-core/src/interpreter'
 import core_clj from './src/clj/core.clj?raw'
 import game from './game.clj?raw'
 
+const start = document.getElementById("start")
+
+start.addEventListener('click', function () {
+  var audioCtx = new AudioContext();
+})
+
+
+
 evalString("(do " + core_clj + ")")
 
 var heading = `<div>
