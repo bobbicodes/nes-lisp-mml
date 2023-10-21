803742c93b00e4a1ebad030e82814d0586ce6f3c
Bobbi Towers <btowers793@gmail.com>
2023-10-21 14:26:05 -0700

    extend grid to entire screen

--- main.js
+++ main.js
@@ -4,17 +4,23 @@ import { EditorState } from '@codemirror/state'
 import { clojure } from "./src/clojure"
 
 let editorState = EditorState.create({
-  doc: `(defn make-path [points]
-  (str "M" (apply str (interpose " " points))))
+  doc: `(defn isometric->screen [x y]
+  [(* (- x y)  (/ 1 2))
+   (* (+ x y)  (/ 1 4))])
+
+(defn make-path [points]
+    (str "M" (apply str (interpose " " points)))))
 
 (def points
   (apply str
-    (for [x (range 0 150 40) y (range 0 150 40)]
-      (make-path [(+ x 10) (+ y 50)
-                  (+ x 10) (+ y 10)
-                  (+ x 50) (+ y 10)
-                  (+ x 50) (+ y 50)
-                  (+ x 10) (+ y 50)]))))
+    (for [x (range 0 850 40) y (range 0 850 40)]
+      (make-path 
+        (flatten
+        [(isometric->screen (+ x 480) (+ y -380))
+         (isometric->screen (+ x 480) (+ y -420))
+         (isometric->screen (+ x 520) (+ y -420))
+         (isometric->screen (+ x 520) (+ y -380))
+         (isometric->screen (+ x 480) (+ y -380))])))))
 
 (let [_ (clear-svg)]
   (append-path points))`,
