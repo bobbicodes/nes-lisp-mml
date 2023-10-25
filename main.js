910264f295315ffb5e6ad0b0b46bef2b8fe7e903
Bobbi Towers <btowers793@gmail.com>
2023-10-24 21:48:36 -0700

    vibrato demo

--- main.js
+++ main.js
@@ -5,53 +5,19 @@ import { clojure } from "./src/clojure"
 import confuzion from './confuzion.json'
 
 let editorState = EditorState.create({
-  doc: `(play (pulse0-seq [{:pitch 60
-    :time 0 
-    :length 3
-    :vibrato {:speed 3
-              :depth 3}}]))
-  
-(play (pulse0-seq [{:pitch 60
-    :time 0 
-    :length 1
-    :vibrato {:speed 3
-              :depth 3}}
-   {:pitch 55
-    :time 1
-    :length 1
-    :vibrato {:speed 3
-              :depth 3}}
-   {:pitch 51
-    :time 2 
-    :length 1
-    :vibrato {:speed 3
-              :depth 3}}
-   {:pitch 48
-    :time 3
-    :length 3
-    :vibrato {:speed 3
-              :depth 3}}]))
-              
-(spit-wav "vibrato-fail" (pulse0-seq [{:pitch 60
-    :time 0 
-    :length 1
-    :vibrato {:speed 3
-              :depth 3}}
-   {:pitch 55
-    :time 1
-    :length 1
-    :vibrato {:speed 3
-              :depth 3}}
-   {:pitch 51
-    :time 2
-    :length 1
-    :vibrato {:speed 3
-              :depth 3}}
-   {:pitch 48
-    :time 3
-    :length 3
-    :vibrato {:speed 3
-              :depth 3}}]))`,
+  doc: `(def pulse0-vib
+  (map #(assoc % :vibrato {:speed 6 :depth 3})
+    pulse-0))
+
+(def buffers
+  [(tri-seq bass)
+  (drum-seq drums)
+  (pulse0-seq pulse0-vib)
+  (pulse2-seq pulse-2)])
+
+(play (mix buffers))
+
+(spit-wav "confuzion-vib.wav" (mix buffers))`,
   extensions: [basicSetup, clojure()]
 })
 
