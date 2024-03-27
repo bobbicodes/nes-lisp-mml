c1eabb03e1bac45c0a7e80319778ae490ada0e86
Bobbi Towers <btowers793@gmail.com>
2024-03-26 20:53:36 -0700

    implement Zeta's sample repeat technique

--- public/audioworklet.js
+++ public/audioworklet.js
@@ -2,6 +2,7 @@ class MyAudioProcessor extends AudioWorkletProcessor {
     constructor() {
         super();
         this.sampleBuffer = new Float32Array(0);
+        this.lastPlayedSample = 0;
         this.port.onmessage = (e) => {
           let mergedBuffer = new Float32Array(this.sampleBuffer.length + e.data.samples.length);
           mergedBuffer.set(this.sampleBuffer);
@@ -12,12 +13,25 @@ class MyAudioProcessor extends AudioWorkletProcessor {
     process (inputs, outputs, parameters) {
        const output = outputs[0]
        const desired_length = output[0].length;
-       output.forEach(channel => {
-         for (let i = 0; i < channel.length; i++) {
-           channel[i] = this.sampleBuffer[i];
-         }
-       })
-       this.sampleBuffer = this.sampleBuffer.slice(desired_length);
+       if (desired_length <= this.sampleBuffer.length) {
+         output.forEach(channel => {
+           for (let i = 0; i < channel.length; i++) {
+             channel[i] = this.sampleBuffer[i];
+           }
+         })
+         // Set the new last played sample, this will be our hold value if we have an underrun
+         this.lastPlayedSample = this.sampleBuffer[desired_length - 1];
+         // Remove those contents from the buffer
+         this.sampleBuffer = this.sampleBuffer.slice(desired_length);
+      } else {
+        // Queue up nothing! Specifically, *repeat* the last sample, to hold the level; this won't
+        // avoid a break in the audio, but it avoids ugly pops
+        output.forEach(channel => {
+          for (let i = 0; i < channel.length; i++) {
+            channel[i] = (this.lastPlayedSample);
+          }
+        })
+      }
        return true
     }
 }
